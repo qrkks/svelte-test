8ceaed14b3e7676b8819ea06d422e3b07067a2a6
qrkks <qrkks3@gmail.com>
2025-06-26 18:12:01 +0800

    2025-06-26 18:11:59

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -3,8 +3,6 @@
         import { goto } from '$app/navigation';
         import { page } from '$app/stores';
         let { error, status } = $page;
-        console.log($page);
-        console.log(error, status);
 </script>
 <div>
     <h1>错误页面</h1>
@@ -12,6 +10,10 @@
     <p>错误信息: {error?.message}</p>
     <p>错误堆栈: {error?.stack}</p>
     <p>错误状态: {status}</p>
+    <div>
+        <h2>错误详情</h2>
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+    </div>
     <button onclick={() => goto('/')}>返回首页</button>
     <button onclick={() => history.back()}>返回上一页</button>
 </div>
